6a910cdc77da3fbd7371514cf4551ad564383ac3
harryjing <houlin_jing@163.com>
2022-03-20 14:14:14 +0800

    rxjava partial complete

--- app/src/main/java/com/harry/myapplication/rxjava/thread/ThreadActivity.java
+++ app/src/main/java/com/harry/myapplication/rxjava/thread/ThreadActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.harry.myapplication.R;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
@@ -15,6 +19,8 @@ import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.internal.disposables.EmptyDisposable;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableSubscribeOn;
 import io.reactivex.rxjava3.plugins.RxJavaPlugins;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -27,6 +33,9 @@ public class ThreadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_thread);
     }
 
+    /**
+     * 以Scheduler.io()为例来分析源码，其他Scheduler逻辑也类似
+     */
     private void test() {
         Observable
                 .create(new ObservableOnSubscribe<String>() {
@@ -35,10 +44,12 @@ public class ThreadActivity extends AppCompatActivity {
 
                     }
                 })
-                // 第二步。会触发里面的线程池
+                // 第二步。返回类型为ObservableSubscribeOn，将IoScheduler包装到ObservableSubscribeOn里
                 .subscribeOn(
                         // 第一步。源码new了IoScheduler，经过多层包装，最终走到对线程池的管控
                         Schedulers.io())
+                // 第三步。走Observable.subscribe()
+                // 进而调用ObservableSubscribeOn.subscribeActual()等等，经过层层包装，最终用线程池执行了Runnable
                 .subscribe(new Observer<String>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
@@ -101,7 +112,7 @@ public class ThreadActivity extends AppCompatActivity {
     // --------------------Schedulers的IO--------------------------
     // 由Schedulers.io()源码可知，IO就是实际的Scheduler
     // 下面是Schedulers的静态代码块的源码。对Schedulers的各种策略进行初始化，RxJavaPlugins的init方法是hook用，实际起作用的是里面的new
-    // 通过IOTask.get()->IoScheduler->IoScheduler.start()->CachedWorkerPool就创建了线程池Executors.newScheduledThreadPool
+    // 通过IOTask.get()->new IoScheduler->IoScheduler.start()->CachedWorkerPool就创建了线程池Executors.newScheduledThreadPool
 //    static {
 //        SINGLE = RxJavaPlugins.initSingleScheduler(new Schedulers.SingleTask());
 //
@@ -114,4 +125,100 @@ public class ThreadActivity extends AppCompatActivity {
 //        NEW_THREAD = RxJavaPlugins.initNewThreadScheduler(new Schedulers.NewThreadTask());
 //    }
 
+    // --------------------subscribeOn()给上面代码切换线程---------------------
+
+    // 下面是ObservableCreate.subscribeOn()源码。用ObservableSubscribeOn将IoScheduler包装了一层
+//    public final Observable<T> subscribeOn(@NonNull Scheduler scheduler) {
+//        Objects.requireNonNull(scheduler, "scheduler is null");
+//        return RxJavaPlugins.onAssembly(new ObservableSubscribeOn<>(this, scheduler));
+//    }
+
+    // 下面是ObservableSubscribeOn.subscribeActual()源码。
+//    public void subscribeActual(final Observer<? super T> observer) {
+    // 将自定义Observer包装成SubscribeOnObserver
+//        final ObservableSubscribeOn.SubscribeOnObserver<T> parent = new ObservableSubscribeOn.SubscribeOnObserver<>(observer);
+//
+//        observer.onSubscribe(parent);
+//
+    // scheduler就是IoScheduler。SubscribeTask是个Runnable任务，可以被IoScheduler的线程池执行
+    // 里面去调用SubscribeOnObserver，进而调用自定义Observer。这行代码完成了用线程池去执行Runnable任务
+//        parent.setDisposable(scheduler.scheduleDirect(new ObservableSubscribeOn.SubscribeTask(parent)));
+//    }
+
+    // 下面是SubscribeTask源码。
+//    final class SubscribeTask implements Runnable {
+//        private final ObservableSubscribeOn.SubscribeOnObserver<T> parent;
+//
+//        SubscribeTask(ObservableSubscribeOn.SubscribeOnObserver<T> parent) {
+//            this.parent = parent;
+//        }
+//
+//        @Override
+//        public void run() {
+    // source就是subscribeOn()中new ObservableSubscribeOn时传进来的ObservableCreate，所以里面的发射器也是在IO线程中执行的
+//            source.subscribe(parent);
+//        }
+//    }
+
+    // 下面是IoScheduler.scheduleDirect()源码。
+//    public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
+    // createWork()在IoScheduler中new EventLoopWorker
+//        final Worker w = createWorker();
+//
+//        final Runnable decoratedRun = RxJavaPlugins.onSchedule(run);
+//
+    // DisposeTask将Runnable包装了一层，使其可以用dispose中断
+//        Scheduler.DisposeTask task = new Scheduler.DisposeTask(decoratedRun, w);
+//
+    // 调用EventLoopWorker.schedule()
+//        w.schedule(task, delay, unit);
+//
+//        return task;
+//    }
+
+    // 下面是EventLoopWorker.schedule()源码。
+//     public Disposable schedule(@NonNull Runnable action, long delayTime, @NonNull TimeUnit unit) {
+//         if (tasks.isDisposed()) {
+//             // don't schedule, we are unsubscribed
+//             return EmptyDisposable.INSTANCE;
+//         }
+//
+    // 这里面就将Runnable交给了threadWorker.scheduleActual()，里面就是线程池ScheduledExecutorService
+//         return threadWorker.scheduleActual(action, delayTime, unit, tasks);
+//     }
+
+
+    // ----------------------observeOn()给下面代码切换线程
+
+    private void test1() {
+        Observable
+                .create(new ObservableOnSubscribe<String>() {
+                    @Override
+                    public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 }
